ae6a1ddb181c3c5edfce300177282b3818690c35
Aashif Malik <aashifmalik776@gmail.com>
2026-04-20 15:15:52 +0530

    Update weatherBackend.js

--- server/weatherBackend.js
+++ server/weatherBackend.js
@@ -32,6 +32,11 @@ let server = http.createServer((req, res) => {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   if (req.method === "OPTIONS") {
+    res.writeHead(200, {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type",
+    });
     res.end();
     return;
   }
